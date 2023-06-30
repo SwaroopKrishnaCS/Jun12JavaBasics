43977e151f428648b8b444352d866e074ecc9768
= <lekshmi867@gmail.com>
2023-06-29 22:56:03 -0400

    If Else

--- src/conditionalStatement/ATMMain.java
+++ src/conditionalStatement/ATMMain.java
@@ -5,20 +5,27 @@ import java.util.Scanner;
 public class ATMMain {
 
 	public static void main(String[] args) {
-		
+
 		Account suryaAccount = new Account();
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		suryaAccount.balanceAmount = 7850.45;
-		
+
 		suryaAccount.overdraftAmount = 10000;
-		
+		suryaAccount.pinNumber = 1234;
+
+		System.out.println("Enter the PIN number");
+
+		suryaAccount.enteredPin = sc.nextInt();
+
 		System.out.println("Enter the amount to withdraw");
-		
+
 		suryaAccount.withdrawAmount = sc.nextInt();
-		
-		suryaAccount.atmWithdrawal();
+
+		suryaAccount.validatePinAndWithdrawCash();
+
+//		suryaAccount.atmWithdrawal();
 
 	}
 
--- src/conditionalStatement/Account.java
+++ src/conditionalStatement/Account.java
@@ -5,20 +5,36 @@ public class Account {
 	double balanceAmount;
 
 	int withdrawAmount;
-	
+
 	int overdraftAmount;
-	
-	//If customer has enough balance (balanceAmount >= withdrawAmount)
-	//(OR) ||
-	//If the customer has enough overDraft (overdraft>=withdrawamount)
-	
+
+	int pinNumber;
+
+	int enteredPin;
+
+	// If customer has enough balance (balanceAmount >= withdrawAmount)
+	// (OR) ||
+	// If the customer has enough overDraft (overdraft>=withdrawamount)
 
 	void atmWithdrawal() {
 
-		if (balanceAmount >= withdrawAmount||overdraftAmount>=withdrawAmount) {
+		if (balanceAmount >= withdrawAmount || overdraftAmount >= withdrawAmount) {
 			System.out.println("Customer can withdraw cash");
 		} else {
 			System.out.println("Not enough balance");
+		}
+	}
+
+	void validatePinAndWithdrawCash() {
+		if (enteredPin == pinNumber) {
+			System.out.println("PIN is correct");
+			if (balanceAmount >= withdrawAmount || overdraftAmount >= withdrawAmount) {
+				System.out.println("Customer can withdraw cash");
+			} else {
+				System.out.println("Not enough balance");
+			}
+		}else {
+			System.out.println("Invalid PIN");
 		}
 	}
 
