9dd27f100ceb77d452fba70ff395260341df7313
manvirsinghparmar <38955635+manvirsinghparmar@users.noreply.github.com>
2023-07-26 16:15:23 -0400

    Feature (#4)
    * Methods added
    
    * Constructor
    
    * Demo Project
    
    * Enum

---
+++ src/enumExample/Passenger.java
@@ -0,0 +1,14 @@
+package enumExample;
+
+public class Passenger {
+
+	String nameOfPassenger;
+	String emailOfPassenger;
+	Seat seatOfPassenger;
+
+	public Passenger(String name, String email, Seat seat) {
+		nameOfPassenger = name;
+		emailOfPassenger = email;
+		seatOfPassenger = seat;
+	}
+}
