f59c0ea36082941103caa1f372dd87ab150c937a
Manvir <manvir.singh.parmar@gmail.com>
2023-08-09 23:01:18 -0400

    Added Inheritance

---
+++ src/inheritanceTwo/MacDonaldJapan.java
@@ -0,0 +1,13 @@
+package inheritanceTwo;
+
+public class MacDonaldJapan extends MacDonaldUS {
+
+	
+	public void provideBurgers() {
+		System.out.println("Provider Ham Burgers to Customer");
+	}
+	
+	public void provideFries() {
+		System.out.println("Provider Fries fried in Fish Oil to Customer");
+	}
+}
