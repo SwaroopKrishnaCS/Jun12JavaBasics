cebfc5b900c59943d7324a4c1334181c05bd2178
manvirsinghparmar <38955635+manvirsinghparmar@users.noreply.github.com>
2023-08-29 23:17:31 -0400

    Feature (#14)
    * Methods added
    
    * Constructor
    
    * Demo Project
    
    * Enum
    
    * String methods
    
    * Compil time polymorphism
    
    * Encap
    
    * Merge branch 'master' of
    https://github.com/SwaroopKrishnaCS/Jun12JavaBasics.git
    
    * Added Inheritance
    
    * Macdonald
    
    * Static and Interface
    
    * Interface continued
    
    * Casting Stuff
    
    * Exceptions
    
    * Collections

---
+++ src/collectionsExample/HashMapExample.java
@@ -0,0 +1,21 @@
+package collectionsExample;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+
+	public static void main(String[] args) {
+		
+		
+		HashMap<Integer, String> hm=new HashMap<>();
+		
+		
+		hm.put(101, "Aman");
+		hm.put(201, "Chaman");
+		
+		
+		System.out.println(hm.get(101));
+
+	}
+
+}
